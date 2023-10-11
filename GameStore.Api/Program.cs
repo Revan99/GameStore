58958ad041dd7a9345d67fe1b69c509b7eea6e61
Revan99 <revan.sarbast@dit.gov.krd>
2023-10-11 14:17:11 +0300

    add game entity

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,32 @@
+using GameStore.Api.Entities;
+
+List <Game> games = new(){
+    new Game(){
+        Id=1,
+        Name = "Street fight",
+        Genre = "Fighting",
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991, 2,1),
+        ImageUri = "https://placehold.co/100",
+    },
+    new Game(){
+        Id=2,
+        Name = "Final fantasy XIV",
+        Genre = "Role playing",
+        Price = 59.99M,
+        ReleaseDate = new DateTime(2010, 9, 30),
+        ImageUri = "https://placehold.co/100",
+    },
+    new Game(){
+        Id=3,
+        Name = "FIFA 23",
+        Genre = "Sport",
+        Price = 69.99M,
+        ReleaseDate = new DateTime(2022, 9, 27),
+        ImageUri = "https://placehold.co/100",
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
