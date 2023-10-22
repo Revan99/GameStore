41664c2906ef459a72814e11f992ff3186e27273
Revan Sarbast Salih <revansarbast8@gmail.com>
2023-10-22 08:40:43 +0300

    add repository pattern

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -6,32 +6,6 @@ public static class GamesEndpoints
 {
     const string GetGameEndPointName = "GetGame";
 
-    static List<Game> games = new(){
-    new Game(){
-        Id=1,
-        Name = "Street fight",
-        Genre = "Fighting",
-        Price = 19.99M,
-        ReleaseDate = new DateTime(1991, 2,1),
-        ImageUri = "https://placehold.co/100",
-    },
-    new Game(){
-        Id=2,
-        Name = "Final fantasy XIV",
-        Genre = "Role playing",
-        Price = 59.99M,
-        ReleaseDate = new DateTime(2010, 9, 30),
-        ImageUri = "https://placehold.co/100",
-    },
-    new Game(){
-        Id=3,
-        Name = "FIFA 23",
-        Genre = "Sport",
-        Price = 69.99M,
-        ReleaseDate = new DateTime(2022, 9, 27),
-        ImageUri = "https://placehold.co/100",
-    }
-};
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/games")
